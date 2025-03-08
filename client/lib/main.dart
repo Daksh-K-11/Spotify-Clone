b0a0b00bdcb9b30789e77d5ecd857c74cda50bf7
Daksh-K-11 <khinvasara.daksh@gmail.com>
2025-03-09 00:57:27 +0530

    scaffold theme

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:client/core/theme/theme.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.darkThemeMode,
       home: const SignupPage(),
     );
   }
