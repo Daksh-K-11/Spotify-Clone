8d931455a13f60c766b2333332332eca91017fac
Daksh-K-11 <khinvasara.daksh@gmail.com>
2025-03-16 14:06:42 +0530

    utils of add image and music

--- client/lib/core/utils.dart
+++ client/lib/core/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:client/core/widgets/animated_snackbar_content.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String content, bool isSuccess) {
@@ -13,4 +16,34 @@ void showSnackBar(BuildContext context, String content, bool isSuccess) {
         ),
       ),
     );
+}
+
+Future<File?> pickAudio() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+    );
+
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<File?> pickImage() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+    );
+
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
 }
