2dde65141b4cda364c331472e2938f35ec6ed2b0
Daksh-K-11 <khinvasara.daksh@gmail.com>
2025-03-18 18:08:27 +0530

    music slab ui

--- client/lib/features/home/view/widgets/music_slab.dart
+++ client/lib/features/home/view/widgets/music_slab.dart
@@ -16,73 +16,101 @@ class MusicSlab extends ConsumerWidget {
       return const SizedBox();
     }
 
-    return Container(
-      height: 66,
-      width: MediaQuery.of(context).size.width - 16,
-      decoration: BoxDecoration(
-        color: hexToColor(currentSong.hex_code),
-        borderRadius: BorderRadius.circular(4),
-      ),
-      padding: const EdgeInsets.all(9),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
+    return Stack(
+      children: [
+        Container(
+          height: 66,
+          width: MediaQuery.of(context).size.width - 16,
+          decoration: BoxDecoration(
+            color: hexToColor(currentSong.hex_code),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          padding: const EdgeInsets.all(9),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                width: 48,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      currentSong.thumbnail_url,
+              Row(
+                children: [
+                  Container(
+                    width: 48,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          currentSong.thumbnail_url,
+                        ),
+                      ),
+                      borderRadius: BorderRadius.circular(4),
                     ),
                   ),
-                  borderRadius: BorderRadius.circular(4),
-                ),
+                  const SizedBox(width: 8),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        currentSong.song_name,
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        currentSong.artist,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Pallete.subtitleText,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              const SizedBox(width: 8),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Text(
-                    currentSong.song_name,
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w500),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      CupertinoIcons.heart,
+                      color: Pallete.whiteColor,
+                    ),
                   ),
-                  Text(
-                    currentSong.artist,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      color: Pallete.subtitleText,
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      CupertinoIcons.play_fill,
+                      color: Pallete.whiteColor,
                     ),
                   ),
                 ],
               )
             ],
           ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  CupertinoIcons.heart,
-                  color: Pallete.whiteColor,
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  CupertinoIcons.play_fill,
-                  color: Pallete.whiteColor,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 8,
+          child: Container(
+            width: 20,
+            height: 2,
+            decoration: BoxDecoration(
+              color: Pallete.whiteColor,
+              borderRadius: BorderRadius.circular(7),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 8,
+          child: Container(
+            width: MediaQuery.of(context).size.width - 32,
+            height: 2,
+            decoration: BoxDecoration(
+              color: Pallete.inactiveSeekColor,
+              borderRadius: BorderRadius.circular(7),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
