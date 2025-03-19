4fb23f4770ec2edac01950a3aadadcf63a6ae90d
Daksh-K-11 <khinvasara.daksh@gmail.com>
2025-03-19 19:44:20 +0530

    ui of music player page done

---
+++ client/lib/features/home/view/widgets/music_player.dart
@@ -0,0 +1,205 @@
+import 'package:client/core/providers/current_song_notifier.dart';
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MusicPlayer extends ConsumerWidget {
+  const MusicPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentSong = ref.watch(currentSongNotifierProvider);
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            hexToColor(currentSong!.hex_code),
+            const Color(0xff121212),
+          ],
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Scaffold(
+        backgroundColor: Pallete.transparentColor,
+        appBar: AppBar(
+          backgroundColor: Pallete.transparentColor,
+          leading: Transform.translate(
+            offset: const Offset(-15, 0),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Image.asset(
+                'assets/images/pull-down-arrow.png',
+                color: Pallete.whiteColor,
+              ),
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 30,
+                ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(currentSong!.thumbnail_url),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            currentSong.song_name,
+                            style: const TextStyle(
+                              color: Pallete.whiteColor,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Text(
+                            currentSong.artist,
+                            style: const TextStyle(
+                              color: Pallete.subtitleText,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Expanded(child: SizedBox()),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          CupertinoIcons.heart,
+                          color: Pallete.whiteColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Column(
+                    children: [
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          activeTrackColor: Pallete.whiteColor,
+                          inactiveTrackColor:
+                              Pallete.whiteColor.withOpacity(0.117),
+                          thumbColor: Pallete.whiteColor,
+                          trackHeight: 4,
+                          overlayShape: SliderComponentShape.noOverlay,
+                        ),
+                        child: Slider(
+                          value: 0.5,
+                          onChanged: (val) {},
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '0:05',
+                            style: TextStyle(
+                              color: Pallete.subtitleText,
+                              fontSize: 13,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                          const Expanded(child: SizedBox()),
+                          Text(
+                            '0:10',
+                            style: TextStyle(
+                              color: Pallete.subtitleText,
+                              fontSize: 13,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Image.asset(
+                              'assets/images/shuffle.png',
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Image.asset(
+                              'assets/images/previus-song.png',
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              CupertinoIcons.play_circle_fill,
+                            ),
+                            iconSize: 80,
+                            color: Pallete.whiteColor,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Image.asset(
+                              'assets/images/next-song.png',
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Image.asset(
+                              'assets/images/repeat.png',
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Image.asset(
+                              'assets/images/connect-device.png',
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                          const Expanded(child: SizedBox()),
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Image.asset(
+                              'assets/images/playlist.png',
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
