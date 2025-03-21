bbecbaaad70d828cd762102a089ba0f6bf45925e
Daksh-K-11 <khinvasara.daksh@gmail.com>
2025-03-21 20:18:18 +0530

    library

--- client/lib/features/home/view/pages/library_page.dart
+++ client/lib/features/home/view/pages/library_page.dart
@@ -1,3 +1,6 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/widgets/loader.dart';
+import 'package:client/features/home/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +9,42 @@ class LibraryPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return ref.watch(getFavSongsProvider).when(
+          data: (data) {
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final song = data[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(song.thumbnail_url),
+                    radius: 35,
+                    backgroundColor: Pallete.backgroundColor,
+                  ),
+                  title: Text(
+                    song.song_name,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  subtitle: Text(
+                    song.artist,
+                    style: const TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          error: (error, stackTrace) {
+            return Center(
+              child: Text(error.toString()),
+            );
+          },
+          loading: () => const Loader(),
+        );
   }
 }
